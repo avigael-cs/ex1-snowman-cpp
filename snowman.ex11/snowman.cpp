0a1425f6c79898f9d25b711ceac494e7eae91ddb
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-03-10 11:04:14 +0200

    Add files via upload

---
+++ snowman.ex11/snowman.cpp
@@ -0,0 +1,11 @@
+#include "snowman.hpp"
+#include <string>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+namespace ariel { 
+    string snowman(int num) {
+        return "Hello! world";
+    }
+}
